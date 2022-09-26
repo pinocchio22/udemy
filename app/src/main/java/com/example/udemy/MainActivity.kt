653e3d96b1cade3b5392a0705e54cf2f09ea1bd2
pinocchio22 <vviian.2@gmail.com>
2022-09-26 20:57:56 +0900

    solution3

--- app/src/main/java/com/example/udemy/MainActivity.kt
+++ app/src/main/java/com/example/udemy/MainActivity.kt
@@ -8,13 +8,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        solution1(2, arrayOf("desk1 request", "desk2 request", "desk3 request", "desk3 request", "desk1 release", "desk3 request"))
+//        solution1(2, arrayOf("desk1 request", "desk2 request", "desk3 request", "desk3 request", "desk1 release", "desk3 request"))
         // desk1 192.168.0.1
 
 //        solution2("11111")
         // 1DBACB
 
-//        solution3(4, intArrayOf(3,2,1,4))
+        solution3(4, intArrayOf(3,2,1,4))
         // 3
     }
 
@@ -66,8 +66,6 @@ class MainActivity : AppCompatActivity() {
         return check
     }
 
-
-
     fun solution2(src: String): String {
         var answer = ""
         val numbermap = mutableMapOf<String, Char>()
@@ -89,5 +87,60 @@ class MainActivity : AppCompatActivity() {
 
         answer += numbermap["$cnt"]
         return answer
+    }
+
+    fun solution3(n: Int, result: IntArray): Int {
+        // n: 4 , result: (3,2,1,4)
+        var answer = 0
+        var horizon = 0
+        var line = Array(n-1){"0" to false}
+        // line = n-1개
+        // n과 n+1 사이에 가로줄이 생기면 n
+        // 1부터 n-1까지의 수로 만들 수 있는 모든 순열 구하기
+        // ((1,2),(2,3),(1,2))
+        var new = combination(n)
+        for (i in 0..3) {
+            var location = 0
+
+            //1
+            new.forEach {
+                location = i+1
+                // it = (1,2)
+                if (it[0] == location) {
+                    location = it[1]
+                    horizon++
+                } else if (it[1] == location) {
+                    location = it[0]
+                    horizon++
+                }
+            }
+
+            //2
+            for (i in new.indices) {
+                if (i == 1)  location = i+1
+
+                if (new[i][0] == location) {
+                    location = new[i][1]
+                    horizon++
+                } else if (new[i][1] == location) {
+                    location = new[i][0]
+                    horizon++
+                }
+            }
+        }
+
+        // 순열의 원소가 적은순으로 탐색
+        // 시작 위치를 변수로 만들고 이동할때마다 현재의 위치를 변경하고 horizon++
+        // result와 일치하는 결과가 나오면 탐색을 중지하고 그때의 horizon 리턴
+
+        return answer
+    }
+
+    fun combination(n: Int): List<List<Int>> {
+        var list: List<List<Int>>
+
+        while ()
+
+        return list
     }
 }
